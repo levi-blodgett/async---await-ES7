cd459238685cb37d6fd54e6a873784268960d336
Levi <levi_blodgett@outlook.com>
2019-01-21 20:39:45 -0600

    Used async and await to practice

---
+++ app.js
@@ -0,0 +1,36 @@
+// // By adding async in front of the function, it then returns a promise
+// async function myFunc() {
+//   // return 'Hello';
+
+//   const promise = new Promise((resolve, reject) => {
+//     setTimeout(() => resolve('Hello'), 1000);
+//   });
+
+//   const error = false;
+
+//   if(!error) {
+//     const res = await promise; // wait until promise is resolved
+//     return res; // Once promise is resolved, it send the response
+//   } else {
+//     await Promise.reject(new Error('Something went wrong....'))
+//   }
+
+// }
+
+// myFunc()
+// .then(res => console.log(res))
+// .catch(err => console.log(err));
+
+const getUsers = async () => {
+  // await response of the fetch call
+  const response = await fetch('https://jsonplaceholder.typicode.com/users');
+
+  // Only proceed once it is resolved
+  const data = await response.json();
+
+  // Only proceed once second promise is resolved
+  return data;
+}
+
+getUsers()
+.then(users => console.log(users));
